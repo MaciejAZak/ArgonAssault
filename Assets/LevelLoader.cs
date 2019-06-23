6c07c93181609f2eeddb486453dc952a7b085615
Zh00c3q <maciej.a.zak@gmail.com>
2019-06-23 10:33:26 +0200

    Splash screen & initial music

---
+++ Assets/LevelLoader.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class LevelLoader : MonoBehaviour {
+
+    void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+    }
+    // Use this for initialization
+    void Start () {
+        Invoke("LoadGame", 2f);
+    }
+	
+void LoadGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
